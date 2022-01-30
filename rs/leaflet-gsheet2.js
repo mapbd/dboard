9834b7159188340455fedcc991f9589e147400ee
Atikur Rahman <arahman.dcc2@gmail.com>
2022-01-30 10:56:18 +0600

    Update leaflet-gsheet2.js

--- rs/leaflet-gsheet2.js
+++ rs/leaflet-gsheet2.js
@@ -10,8 +10,8 @@
 // the first is the geometry layer and the second the points
 let geomURL =
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4tE0qWIHUkmdKp0VXIY6q5QjEk28Is3tPEUQaNjcFMwwS8oKjmg_-RUU3nYW26BjnYfhmEWSopu5C/pub?gid=0&single=true&output=csv";
-let pointsURL =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSFQw9sVY16eQmN5TIjOH7CUaxeZnl_v6LcdE2goig1pSe9I3hipeOn1sOwmC4fS0AURefRWwcKExct/pub?output=csv";
+// let pointsURL =
+//   "https://docs.google.com/spreadsheets/d/e/2PACX-1vSFQw9sVY16eQmN5TIjOH7CUaxeZnl_v6LcdE2goig1pSe9I3hipeOn1sOwmC4fS0AURefRWwcKExct/pub?output=csv";
 
 window.addEventListener("DOMContentLoaded", init);
 
@@ -24,11 +24,11 @@ let panelID = "my-info-panel";
  */
 function init() {
   // Create a new Leaflet map centered on the continental US
-  map = L.map("map").setView([51.5, -0.1], 14);
+  map = L.map("map").setView([25.39098,88.99309], 16);
 
   // This is the Carto Positron basemap
   L.tileLayer(
-    "https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}{r}.png",
+    "Tiles/{z}/{x}/{y}.png",
     {
       attribution:
         "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://cartodb.com/attributions'>CartoDB</a>",
@@ -64,11 +64,11 @@ function init() {
     header: true,
     complete: addGeoms,
   });
-  Papa.parse(pointsURL, {
-    download: true,
-    header: true,
-    complete: addPoints,
-  });
+  // Papa.parse(pointsURL, {
+  //   download: true,
+  //   header: true,
+  //   complete: addPoints,
+  // });
 }
 
 /*
@@ -132,75 +132,75 @@ function addGeoms(data) {
   }).addTo(map);
 }
 
-/*
- * addPoints is a bit simpler, as no GeoJSON is needed for the points
- */
-function addPoints(data) {
-  data = data.data;
-  let pointGroupLayer = L.layerGroup().addTo(map);
+// /*
+//  * addPoints is a bit simpler, as no GeoJSON is needed for the points
+//  */
+// function addPoints(data) {
+//   data = data.data;
+//   let pointGroupLayer = L.layerGroup().addTo(map);
 
-  // Choose marker type. Options are:
-  // (these are case-sensitive, defaults to marker!)
-  // marker: standard point with an icon
-  // circleMarker: a circle with a radius set in pixels
-  // circle: a circle with a radius set in meters
-  let markerType = "marker";
+//   // Choose marker type. Options are:
+//   // (these are case-sensitive, defaults to marker!)
+//   // marker: standard point with an icon
+//   // circleMarker: a circle with a radius set in pixels
+//   // circle: a circle with a radius set in meters
+//   let markerType = "marker";
 
-  // Marker radius
-  // Wil be in pixels for circleMarker, metres for circle
-  // Ignore for point
-  let markerRadius = 100;
+//   // Marker radius
+//   // Wil be in pixels for circleMarker, metres for circle
+//   // Ignore for point
+//   let markerRadius = 100;
 
-  for (let row = 0; row < data.length; row++) {
-    let marker;
-    if (markerType == "circleMarker") {
-      marker = L.circleMarker([data[row].lat, data[row].lon], {
-        radius: markerRadius,
-      });
-    } else if (markerType == "circle") {
-      marker = L.circle([data[row].lat, data[row].lon], {
-        radius: markerRadius,
-      });
-    } else {
-      marker = L.marker([data[row].lat, data[row].lon]);
-    }
-    marker.addTo(pointGroupLayer);
+//   for (let row = 0; row < data.length; row++) {
+//     let marker;
+//     if (markerType == "circleMarker") {
+//       marker = L.circleMarker([data[row].lat, data[row].lon], {
+//         radius: markerRadius,
+//       });
+//     } else if (markerType == "circle") {
+//       marker = L.circle([data[row].lat, data[row].lon], {
+//         radius: markerRadius,
+//       });
+//     } else {
+//       marker = L.marker([data[row].lat, data[row].lon]);
+//     }
+//     marker.addTo(pointGroupLayer);
 
-    // UNCOMMENT THIS LINE TO USE POPUPS
-    //marker.bindPopup('<h2>' + data[row].name + '</h2>There's a ' + data[row].description + ' here');
+//     // UNCOMMENT THIS LINE TO USE POPUPS
+//     //marker.bindPopup('<h2>' + data[row].name + '</h2>There's a ' + data[row].description + ' here');
 
-    // COMMENT THE NEXT GROUP OF LINES TO DISABLE SIDEBAR FOR THE MARKERS
-    marker.feature = {
-      properties: {
-        name: data[row].name,
-        description: data[row].description,
-      },
-    };
-    marker.on({
-      click: function (e) {
-        L.DomEvent.stopPropagation(e);
-        document.getElementById("sidebar-title").innerHTML =
-          e.target.feature.properties.name;
-        document.getElementById("sidebar-content").innerHTML =
-          e.target.feature.properties.description;
-        sidebar.open(panelID);
-      },
-    });
-    // COMMENT UNTIL HERE TO DISABLE SIDEBAR FOR THE MARKERS
+//     // COMMENT THE NEXT GROUP OF LINES TO DISABLE SIDEBAR FOR THE MARKERS
+//     marker.feature = {
+//       properties: {
+//         name: data[row].name,
+//         description: data[row].description,
+//       },
+//     };
+//     marker.on({
+//       click: function (e) {
+//         L.DomEvent.stopPropagation(e);
+//         document.getElementById("sidebar-title").innerHTML =
+//           e.target.feature.properties.name;
+//         document.getElementById("sidebar-content").innerHTML =
+//           e.target.feature.properties.description;
+//         sidebar.open(panelID);
+//       },
+//     });
+//     // COMMENT UNTIL HERE TO DISABLE SIDEBAR FOR THE MARKERS
 
-    // AwesomeMarkers is used to create fancier icons
-    let icon = L.AwesomeMarkers.icon({
-      icon: "info-circle",
-      iconColor: "white",
-      markerColor: data[row].color,
-      prefix: "fa",
-      extraClasses: "fa-rotate-0",
-    });
-    if (!markerType.includes("circle")) {
-      marker.setIcon(icon);
-    }
-  }
-}
+//     // AwesomeMarkers is used to create fancier icons
+//     let icon = L.AwesomeMarkers.icon({
+//       icon: "info-circle",
+//       iconColor: "white",
+//       markerColor: data[row].color,
+//       prefix: "fa",
+//       extraClasses: "fa-rotate-0",
+//     });
+//     if (!markerType.includes("circle")) {
+//       marker.setIcon(icon);
+//     }
+//   }
+// }
 
 /*
  * Accepts any GeoJSON-ish object and returns an Array of
@@ -237,4 +237,15 @@ function parseGeom(gj) {
     }
     return [{ type: "Feature", geometry: { type: type, coordinates: gj } }];
   }
+
+  // Set different polygon fill colors based on number of quarantined
+  polygonLayer.eachLayer(function (layer) {
+    let d = layer.feature.properties.statuscode;
+    let fc = d == 2 ? 'red' :
+          d == 1   ? 'blue' :
+          d == 0    ? 'green' :
+          '#FFFFFF';
+    layer.setStyle({fillColor: fc});
+    layer.feature.fill_color = fc;  // Save color to use again after mouseout
+  });
 }
